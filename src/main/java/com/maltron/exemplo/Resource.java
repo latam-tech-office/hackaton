95ff6a8bf43c5d557f93771afc1a428f5d642226
Mauricio Leal <maltron@gmail.com>
2017-05-11 18:54:05 -0300

    Second Commit

--- src/main/java/com/maltron/exemplo/Resource.java
+++ src/main/java/com/maltron/exemplo/Resource.java
@@ -1,25 +1,22 @@
 package com.maltron.exemplo;
 
-import java.io.Serializable;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import javax.ejb.EJB;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-/**
- *
- * @author Mauricio "Maltron" Leal <maltron at gmail dot com> */
 @Path("/exemplo")
-public class Resource implements Serializable {
-
-    private static final Logger LOG = Logger.getLogger(Resource.class.getName());
+public class Resource {
     
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public Response myName() {
-        return Response.ok("Maltron").build();
+    @EJB
+    private Service service;
+    
+    @GET @Path("/{ID}")
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public Response findByID(@PathParam("ID")long ID) throws NotFoundException, ServerErrorException {
+        return Response.ok(service.findByID(ID)).build();
     }
 }
